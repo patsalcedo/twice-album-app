a0b311f3db888894247f6a300e4a49567c603964
Patricia <patricia.salcedo15@outlook.com>
2020-09-01 22:17:54 +1200

    Changed css and added api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
-import logo from "./logo.svg";
+import twicelogo from "./twicelogo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={twicelogo} className="App-logo" alt="logo" />
+        <p>TWICE Album App</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtu.be/mH0_XpSHkZo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          New Twice App!
+          Stream MORE & MORE
         </a>
       </header>
+      <p className="names">
+        NAYEON | JEONGYEON | MOMO | SANA | JIHYO
+        <br />
+        <t />
+        MINA | DAHYUN | CHAEYOUNG | TZUYU
+      </p>
     </div>
   );
 }
